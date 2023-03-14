12a951a0227c777d3d2df7e78fb2fd8ef1eecba3
Karambasss <m.papa.09@mail.ru>
2023-03-15 01:23:22 +0300

    editCommit

--- MovieApplication/app/src/main/java/com/mywebsite/karambasss/view/adapters/CustomAdapter.kt
+++ MovieApplication/app/src/main/java/com/mywebsite/karambasss/view/adapters/CustomAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.mywebsite.karambasss.R
 import com.mywebsite.karambasss.data.Item
